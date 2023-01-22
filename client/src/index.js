f490a129f6e07051e22829122ae1d8a6dbdc1ee7
Dáel Muñiz <daelvn@gmail.com>
2023-01-22 08:45:52 +0000

    add routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import Setup from "./views/admin/Setup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Import Views
+import Homestuck from "./views/Homestuck";
+
+// TODO: route all other views
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Setup />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homestuck />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
