ad370ea317da2e10cefb8f995de8e786eab3e281
Dáel Muñiz <daelvn@gmail.com>
2023-01-22 08:46:16 +0000

    preparation for api endpoints

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,16 +1,43 @@
 var express = require("express");
+var multer = require("multer");
 var router = express.Router();
 
 // Import controller
 const { initialSetup } = require("../controllers");
 
-/* GET users listing. */
-router.get("/init", function (req, res, next) {
+// Create multer uploader
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // TODO: make sure this is the correct place
+    cb(null, "../static/img");
+  },
+  filename: function (req, file, cb) {
+    // TODO: make sure filenames dont conflict
+    console.log(req);
+    cb(null, "example.png");
+  },
+});
+const upload = multer({ storage: storage });
+
+// POST /init : Create initial setup
+// FIXME DO NOT EXPOSE IN PROD
+router.post("/init", function (req, res, next) {
   async function body() {
     await initialSetup();
   }
   body();
   res.send("connecting to mongodb...");
+});
+
+// POST /create : Create new page
+//   id*      : Unique ID for the page
+//   title*   : Title of the page
+//   password : Password for editing later
+//   next     : Array of links
+//   panel    : Image
+router.post("/create", upload.single("panel"), function (req, res, next) {
+  // TODO create panel
+  res.send({ id: "1" });
 });
 
 module.exports = router;
