46555200522caf007cfd8dca3bca0fc7dbb5f29e
Dáel Muñiz <daelvn@gmail.com>
2023-01-22 06:22:44 +0000

    Restructure client to be separate

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Setup from "./views/admin/Setup";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Setup />
+  </React.StrictMode>
+);
