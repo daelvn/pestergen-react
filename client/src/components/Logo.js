2bd095f274dfec88d644a22db4f9a93b42ad9653
Dáel Muñiz <daelvn@gmail.com>
2023-01-22 08:45:57 +0000

    fixme notice

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,4 +1,4 @@
-const LOGO_URI = "/static/home/tezi.gif";
+const LOGO_URI = "/static/home/tezi.gif"; //FIXME: wrong location
 
 export default function Logo() {
   return <img src={LOGO_URI} className="App-logo" alt="logo" />;
