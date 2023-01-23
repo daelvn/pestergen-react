03424bc688c4a414fe9691673df6dbe3b8de0cd6
Dáel Muñiz <daelvn@gmail.com>
2023-01-23 05:33:05 +0000

    Serve React app from Express

--- server/app.js
+++ server/app.js
@@ -15,9 +15,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+//app.use("/", indexRouter);
 app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
