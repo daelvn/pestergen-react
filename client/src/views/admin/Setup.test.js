683f982462288b62f1b44b065b2d689f1898d946
Dáel Muñiz <daelvn@gmail.com>
2023-01-23 06:10:11 +0000

    Delete tests

--- client/src/views/admin/Setup.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Setup from "./Setup";
-
-test("renders learn react link", () => {
-  render(<Setup />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
