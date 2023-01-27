d992b3562eda77eaf4ade879ce295d84e3146cf6
Dáel Muñiz <daelvn@gmail.com>
2023-01-27 23:02:09 +0000

    Remove logging

--- client/src/components/admin/CreateHomepageButton.js
+++ client/src/components/admin/CreateHomepageButton.js
@@ -1,6 +1,5 @@
 export default function CreateHomepageButton() {
   function handleClick() {
-    console.log("Calling initialSetup!");
     //initialSetup();
   }
   return <button onClick={handleClick}>Create Homepage</button>;
