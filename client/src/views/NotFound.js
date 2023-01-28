58975b7082932901db42f587c9f174c4dce11fb9
Dáel Muñiz <daelvn@gmail.com>
2023-01-28 10:36:15 +0000

    Finalize error page

--- client/src/views/NotFound.js
+++ client/src/views/NotFound.js
@@ -3,8 +3,10 @@ import "../index.css";
 export default function NotFound() {
   return (
     <div align="center" className="comic">
+      <img src="/static/howhigh.jpg" />
       <h1>uh oh...</h1>
       <p>we cannot found the page. so sorry! ha ha</p>
+      <a href="/">go back HOEM</a>
     </div>
   );
 }
