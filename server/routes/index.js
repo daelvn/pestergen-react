3af602745a318203a56fcdda55d3a18048a1df25
Dáel Muñiz <daelvn@gmail.com>
2023-01-28 20:37:11 +0000

    Update valid routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var path = require("path");
 var router = express.Router();
 
 /* GET home page. */
-router.get(["/", "/create", "/view/*", "/404", "/list"], function (req, res, next) {
+router.get(["/", "/create", "/view/*", "/404", "/list", "/edit/*", "/auth/*"], function (req, res, next) {
   res.sendFile(path.join(__dirname + "/../public/index.html"));
 });
 
