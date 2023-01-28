80f02c57851700a75f3dd945fe3f69a200eae1f3
Dáel Muñiz <daelvn@gmail.com>
2023-01-28 10:37:21 +0000

    Add List to allowed routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,10 @@ var path = require("path");
 var router = express.Router();
 
 /* GET home page. */
-router.get(["/", "/create", "/view/*", "/404"], function (req, res, next) {
+router.get(["/", "/create", "/view/*", "/404", "/list"], function (req, res, next) {
   res.sendFile(path.join(__dirname + "/../public/index.html"));
 });
+
+// TODO redirect nofounds to 404
 
 module.exports = router;
