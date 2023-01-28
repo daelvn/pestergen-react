9328cb19362942c5175512435cea618a2e97c9a8
Dáel Muñiz <daelvn@gmail.com>
2023-01-28 06:04:39 +0000

    Add two new pages

---
+++ client/src/views/NotFound.js
@@ -0,0 +1,10 @@
+import "../index.css";
+
+export default function NotFound() {
+  return (
+    <div align="center" className="comic">
+      <h1>uh oh...</h1>
+      <p>we cannot found the page. so sorry! ha ha</p>
+    </div>
+  );
+}
