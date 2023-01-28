c1b4f548cfdb5ea1f64dfefb5344ced85a10b478
Dáel Muñiz <daelvn@gmail.com>
2023-01-28 09:20:26 +0000

    Create List page

--- client/src/views/PageList.js
+++ client/src/views/PageList.js
@@ -1,1 +1,33 @@
-export default function PageList() {}
+import { CssVarsProvider } from "@mui/joy/styles";
+import Sheet from "@mui/joy/Sheet";
+import CssBaseline from "@mui/joy/CssBaseline";
+import Typography from "@mui/joy/Typography";
+
+export default function PageList() {
+  return (
+    <CssVarsProvider>
+      <CssBaseline />
+      <Sheet
+        sx={{
+          //width: 300,
+          mx: 4, // margin left & right
+          my: 4, // margin top & botom
+          py: 3, // padding top & bottom
+          px: 2, // padding left & right
+          display: "block",
+          //flexDirection: "column",
+          gap: 2,
+          borderRadius: "sm",
+          boxShadow: "md",
+        }}
+        variant="outlined"
+      >
+        <div>
+          <Typography level="h4">
+            <b>See all Pesterlogs</b>
+          </Typography>
+        </div>
+      </Sheet>
+    </CssVarsProvider>
+  );
+}
