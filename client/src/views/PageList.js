cafacbaf9c01d57416720f49186511c12083f97b
Dáel Muñiz <daelvn@gmail.com>
2023-01-28 10:07:40 +0000

    Finish Page view

--- client/src/views/PageList.js
+++ client/src/views/PageList.js
@@ -3,12 +3,31 @@ import Sheet from "@mui/joy/Sheet";
 import CssBaseline from "@mui/joy/CssBaseline";
 import Typography from "@mui/joy/Typography";
 import { Divider, List } from "@mui/joy";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Paginator from "../components/Paginator";
+import PageListContents from "../components/PageListContents";
 
 export default function PageList() {
   const [fetched, setFetched] = useState(false);
   const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(1);
   const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    if (fetched) return;
+    let req = fetch(`/api/list/${page}`, { method: "GET" });
+    req
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.error) {
+          window.alert(`Could not display list! ${json.error}`);
+        } else {
+          setLogs(json.logs);
+          setTotal(json.count);
+          setFetched(true);
+        }
+      });
+  });
 
   return (
     <CssVarsProvider>
@@ -33,7 +52,17 @@ export default function PageList() {
             <b>See all Pesterlogs</b>
           </Typography>
           <Divider />
-          <List></List>
+          <List>
+            <PageListContents logs={logs} />
+          </List>
+          <Paginator
+            total={total}
+            page={page}
+            setPage={(n) => {
+              setPage(n);
+              setFetched(false);
+            }}
+          />
         </div>
       </Sheet>
     </CssVarsProvider>
