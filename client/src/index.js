cf78859330991523e48ad3f3af9e5f36f9eb3f1d
Dáel Muñiz <daelvn@gmail.com>
2023-01-28 06:04:10 +0000

    Improve routing on the client

--- client/src/index.js
+++ client/src/index.js
@@ -5,16 +5,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Import Views
 import Homestuck from "./views/Homestuck";
 import Create from "./views/Create";
+import NotFound from "./views/NotFound";
+
+function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Homestuck id={"home"} />} />
+      <Route path="/view/:id" element={<Homestuck />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/404" element={<NotFound />} />
+    </Routes>
+  );
+}
 
 // TODO: route all other views
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homestuck />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
+      <Router />
     </BrowserRouter>
   </React.StrictMode>
 );
