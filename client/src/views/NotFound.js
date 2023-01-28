e3bde30e22615d171abed4cba19cc134e99c511e
Dáel Muñiz <daelvn@gmail.com>
2023-01-28 20:35:19 +0000

    Fix alt text on NotFound

--- client/src/views/NotFound.js
+++ client/src/views/NotFound.js
@@ -3,7 +3,7 @@ import "../index.css";
 export default function NotFound() {
   return (
     <div align="center" className="comic">
-      <img src="/static/howhigh.jpg" />
+      <img src="/static/howhigh.jpg" alt="" />
       <h1>uh oh...</h1>
       <p>we cannot found the page. so sorry! ha ha</p>
       <a href="/">go back HOEM</a>
