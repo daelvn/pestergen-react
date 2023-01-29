41c5990a41f70c0240f742320a7f5fe0571ec1fa
Dáel Muñiz <daelvn@gmail.com>
2023-01-29 05:17:44 +0000

    Helmet meta tags did not work as expected

--- client/src/views/NotFound.js
+++ client/src/views/NotFound.js
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import "../index.css";
 
 export default function NotFound() {
@@ -7,6 +8,16 @@ export default function NotFound() {
       <h1>uh oh...</h1>
       <p>we cannot found the page. so sorry! ha ha</p>
       <a href="/">go back HOEM</a>
+      <Helmet>
+        <meta name="description" content="paeg not FOUND" />
+        <meta content="Pestergen!" property="og:title" />
+        <meta content="This page doesnt exist. Are you sure you got the right link?" property="og:description" />
+        <meta content="https://pestergen.daelvn.com" property="og:url" />
+        <meta content="https://pestergen.daelvn.com/static/embed.png" property="og:image" />
+        <meta content="#43B581" data-react-helmet="true" name="theme-color" />
+
+        <title>Pestergen!</title>
+      </Helmet>
     </div>
   );
 }
