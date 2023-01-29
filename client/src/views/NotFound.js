7ada62482f7dbb8edb079ea66cdcb0fd03f5a367
Dáel Muñiz <daelvn@gmail.com>
2023-01-29 04:59:28 +0000

    Moved meta tags to a Helmet

--- client/src/views/NotFound.js
+++ client/src/views/NotFound.js
@@ -7,6 +7,16 @@ export default function NotFound() {
       <h1>uh oh...</h1>
       <p>we cannot found the page. so sorry! ha ha</p>
       <a href="/">go back HOEM</a>
+      <Helmet>
+        <meta name="description" content="paeg not FOUND" />
+        <meta content="Pestergen!" property="og:title" />
+        <meta content="This page doesnt exist. Are you sure you got the right link?" property="og:description" />
+        <meta content="https://pestergen.daelvn.com" property="og:url" />
+        <meta content="https://pestergen.daelvn.com/static/embed.png" property="og:image" />
+        <meta content="#43B581" data-react-helmet="true" name="theme-color" />
+
+        <title>Pestergen!</title>
+      </Helmet>
     </div>
   );
 }
